57f0f79641df503d3098aa9e10e7950a815cc4d8
yycx0328 <lyh_oracle@hotmail.com>
2017-05-10 17:00:00 +0800

    dynamic load categories

--- public/js/index.js
+++ public/js/index.js
@@ -1,88 +1,14 @@
 /**
  * Created by lyh_o on 2017/5/7.
  */
-$(function () {
-    $loginBox = $('#loginBox');
-    $registerBox = $('#registerBox');
-    $userInfo = $('#userInfo');
-
-    // 显示注册模块
-    $loginBox.find('a.colMint').on('click',function () {
-        $registerBox.show();
-        $loginBox.hide();
+function categoryClick(categoryid,categoryname) {
+    console.log(categoryid+" "+categoryname);
+    $.ajax({
+        type:'post',
+        url:'/index/category_index',
+        data:{categoryid:categoryid,categoryname:name},
+        dataType:'json',
+        success:function (data) {
+        }
     });
-
-    // 显示登录模块
-    $registerBox.find('a.colMint').on('click',function () {
-        $loginBox.show();
-        $registerBox.hide();
-    });
-
-    // 登录按钮
-    $loginBox.find('button').on('click',function () {
-        $.ajax({
-            type:'post',
-            url:'/api/user/login',
-            data:{
-                username:$loginBox.find('[name="username"]').val(),
-                password:$loginBox.find('[name="password"]').val()
-            },
-            dataType:'json',
-            success:function (data) {
-                console.log(data);
-                if(data.code){
-                    $loginBox.find('.colWarning').html(data.message);
-                }
-                else {
-                    setTimeout(function () {
-                        // $loginBox.hide();
-
-                        $userInfo.find('.username').html(data.userInfo.username);
-                        $userInfo.find('.info').html('你好，欢迎光临我的博客！');
-                        window.location.reload();
-                    }, 1000);
-                }
-            },
-            error:function (err) {
-                console.log(err);
-            }
-        })
-    });
-
-    // 注册按钮
-    $registerBox.find('button').on('click',function () {
-        $.ajax({
-            type:'post',
-            url:'/api/user/register',
-            data:{
-                username:$registerBox.find('[name="username"]').val(),
-                password:$registerBox.find('[name="password"]').val(),
-                repassword:$registerBox.find('[name="repassword"]').val()
-            },
-            dataType:'json',
-            success:function (data) {
-                $registerBox.find('.colWarning').html(data.message);
-                if(!data.code){
-                    setTimeout(function () {
-                        $loginBox.show();
-                        $registerBox.hide();
-                    },1000);
-                }
-            }
-        });
-    });
-
-    $('#logoutBtn').on('click',function () {
-        $.ajax({
-            type:'get',
-            url:'/api/logout',
-            dataType:'json',
-            success:function (data) {
-                $registerBox.find('.colWarning').html(data.message);
-                if(!data.code){
-                    window.location.reload();
-                }
-            }
-        });
-    });
-});
+}
--- routers/index.js
+++ routers/index.js
@@ -3,15 +3,26 @@
  */
 var express = require('express');
 var router = express.Router();
+var Category = require('../models/Categories');
 var jsonResult = {
     code:-1,
     message:'初始化异常'
 };
 
 router.get('/',function (req, res,next) {
-    res.render('index',{
-        userInfo:req.userInfo
+    Category.find({},function (err,categories) {
+        res.render('index',{
+            userInfo:req.userInfo,
+            categories : categories
+        });
     });
+});
+
+router.post('/category_index',function (req,res,next) {
+    console.log(req.body);
+    var categoryid = req.body.categoryid;
+    var categoryname = req.body.categoryname;
+    res.render('category_index',{categoryid:categoryid,categoryname:categoryname});
 });
 
 module.exports = router;

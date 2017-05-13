e3792cbf3631f9cc4cd2d75e57c435a630994664
yycx0328 <lyh_oracle@hotmail.com>
2017-05-13 23:02:59 +0800

    index page dynamic bind data

--- routers/index.js
+++ routers/index.js
@@ -4,26 +4,44 @@
 var express = require('express');
 var router = express.Router();
 var Category = require('../models/Category');
+var Content = require('../models/Content');
 var jsonResult = {
     code:-1,
     message:'初始化异常'
 };
 
 router.get('/',function (req, res,next) {
-    Category.find({},function (err,categories) {
-        res.render('index',{
-            userInfo:req.userInfo,
-            categories : categories
-        });
-    });
-});
-
-router.get('/category_index',function (req,res,next) {
-    var categoryid = req.query.categoryid;
-    var categoryname = req.query.categoryname;
-    res.render('category_index',{
-        categoryid:categoryid,
-        categoryname:categoryname
+    var data = {
+        userInfo:req.userInfo,
+        category:req.query.category||'',
+        categories:[],
+        count:0,
+        page:Number(req.query.page||1),
+        limit:2,
+        pages:0,
+        pageArr:[],
+        contents:[]
+    }
+    var where={};
+    if(data.category!=''){
+        where.category = data.category;
+    }
+    Category.find().then(function (categories) {
+        data.categories = categories;
+        return Content.count(where);
+    }).then(function (count) {
+        data.count = count;
+        data.pages = Math.ceil(data.count/data.limit);
+        for (var i=1; i<=data.pages;i++){
+            data.pageArr.push(i);
+        }
+        data.page = Math.min(data.page,data.pages);
+        data.page = Math.max(data.page,1);
+        var skip = (data.page-1)*data.limit;
+        return Content.find(where).limit(data.limit).skip(skip).populate(['category','user']).sort({createtime:-1});
+    }).then(function (contents) {
+        data.contents = contents;
+        res.render('index',data);
     });
 });
 

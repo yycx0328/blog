dc0267c0609a9b9edafcc2fc7b96115c4e90f39f
yycx0328 <lyh_oracle@hotmail.com>
2017-05-16 17:06:37 +0800

    layout

--- routers/index.js
+++ routers/index.js
@@ -10,18 +10,27 @@ var jsonResult = {
     message:'初始化异常'
 };
 
+router.use(function (req,res,next) {
+    var top = 4;
+    Content.find().sort({createtime:-1}).limit(top).then(function (contents) {
+        req.newestContents = contents;
+        next();
+    });
+});
+
 router.get('/',function (req, res,next) {
     var data = {
         userInfo:req.userInfo,
+        newestContents:req.newestContents,
         category:req.query.category||'',
         categories:[],
         count:0,
         page:Number(req.query.page||1),
-        limit:2,
+        limit:4,
         pages:0,
         pageArr:[],
         contents:[]
-    }
+    };
     var where={};
     if(data.category!=''){
         where.category = data.category;
